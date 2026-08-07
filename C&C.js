5af3f1bf2aae4d15a1f6e7c22aea4df010c37ad0
elishayounger-spec <elisha.younger@hgs.hiddengeniusproject.org>
2026-08-07 10:40:17 -0700

    Rahsaan is the goat

--- C&C.js
+++ C&C.js
@@ -1,6 +1,8 @@
 let model_image = document.getElementById('model-image'); 
+let model = document.getElementById('model-container');
 let price = document.getElementById('price-container'); 
 let range = document.getElementById('range-container'); 
+
 
 const car1 = document.getElementById('car1'); 
 car1.addEventListener('click', () => handle_car_model('car1'));
@@ -14,24 +16,29 @@ car3.addEventListener('click', () => handle_car_model('car3'));
 function handle_car_model(name){
     console.log(name);
     let selected_car = models[name]; 
+    model.textContent = selected_car.model; 
     price.textContent = selected_car.price;
-    range.textContent = selected_car.range; 
+    range.textContent = selected_car.range;
+    
     // model_image.src 
 }
 
 let models = {
     car1: {
-        price: 59995, 
-        range: 230,
+        model: "Volkswagen I.D. Buzz",
+        price: "$59,995", 
+        range: "200+ miles",
         // src: ''
     },
     car2: {
-        price: 53760, 
-        range: 500,
+        model:"Chrysler Pacifica",
+        price: "$53,760", 
+        range: "500+ miles",
     },  
     car3: {
-        price: 43035,
-        range: 400,
+        model:"Toyota Sienna",
+        price: "$41,320",
+        range: "600+ miles",
     },
 
 }   
